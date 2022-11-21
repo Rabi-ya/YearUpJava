a9eade3522e8d337b6edaf2baef0a769dbcb80ae
Rabeel Sharafat <rsharafat@nyc.yearup.org>
2022-11-21 00:13:43 -0500

    Week 3 HW

--- Lectures/week2/com/yearup/week2/homework/Executor.java
+++ Lectures/week2/com/yearup/week2/homework/Executor.java
@@ -1,9 +1,89 @@
 package com.yearup.week2.homework;
+import java.sql.SQLOutput;
 import java.util.Scanner;
+
+import static com.yearup.week2.homework.Executor.PlanetName.planetMoon;
 
 public class Executor {
     static Scanner scanner = new Scanner(System.in);
     static SolarSystemPlanet sol = new SolarSystemPlanet();
+
+    /*Create an anonymous class called CelestialObject. This should implement an interface
+    with the same name and one method to displayCelestialobject().Override the method and display “Comet is a celestiaL
+    object” */
+    interface CelestialObject {
+        void displayCelestialObject();
+    }
+    static CelestialObject celestialobyecto = new CelestialObject() {
+        @Override
+        public void displayCelestialObject() {
+            System.out.println("Comet is a celestial object");
+        }
+    };
+
+    //Create a static class which has one method to display the text “Your planet belongs to Milky way galaxy.”
+    public static class MilkywayGalaxy{
+        public static void displayMilkyway(){
+            System.out.println("Your planet belongs to the Milky way galaxy");
+        }
+    }
+
+    /*Create an Enum class called PlanetName with all planet names and number of moons.
+    Mercury, Earth, Mars, Jupiter, Uranus, Neptune,*/
+    public enum PlanetName{
+        Mercury (0),
+        Earth (0) ,
+        Venus (1),
+        Mars (2),
+        Jupiter (80),
+        Saturn (83),
+        Uranus (27),
+        Neptune (14);
+
+        private int numberofMoons;  //Instance variable
+
+        PlanetName(int numberofMoons) { //Constructor to initialize the instance variable
+            this.numberofMoons=numberofMoons;
+        }
+
+        public static String planetMoon(){
+            System.out.println("Enter planet name: ");
+            String input = scanner.nextLine();
+            return input;
+        }
+
+        public static int returnMoons(PlanetName planet) {
+            int moon=0;
+            switch (planet) {
+                case Mercury:
+                case Venus:
+                    moon = 0;
+                    break;
+                case Earth:
+                    moon = 1;
+                    break;
+                case Mars:
+                    moon = 2;
+                    break;
+                case Jupiter:
+                    moon = 80;
+                    break;
+                case Saturn:
+                    moon = 83;
+                    break;
+                case Uranus:
+                    moon = 27;
+                    break;
+                case Neptune:
+                    moon = 14;
+                    break;
+            }
+            return moon;
+        }
+
+
+
+    }
 
     public static void main(String[] args) {
         String planetName = getPlanetName();
@@ -13,6 +93,10 @@ public class Executor {
         getStar(sol);
         getPos(sol, planetName);
 
+        celestialobyecto.displayCelestialObject();
+        MilkywayGalaxy.displayMilkyway();
+        PlanetName planet = PlanetName.valueOf(planetMoon());
+        System.out.println("The number of moons this planet has is: "+ planet.numberofMoons);
 
     }
 
@@ -42,8 +126,19 @@ public class Executor {
     public static void getPos(SolarSystemPlanet planet1, String planetName) {
         int position = sol.getPos(planetName);
         System.out.println("The position of the planet is: " + position);
+    }}
+    /*interface CelestialObject {
+        void displayCelestialObject();
     }
 
 
+    static CelestialObject celestialobyecto = new CelestialObject() {
+        @Override
+        public void displayCelestialObject() {
+            System.out.println("Comet is a celestial object");
+        }
 
-    }
+
+
+    };
+     */
